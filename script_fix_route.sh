605c8918d70023e8f9b94e2dc601db791e6426cb
tuanta <88d52bdba0366127fffca9dfa93895@users.noreply.github.com>
2015-08-09 22:16:01 +0700

    Update script_fix_route.sh

--- script_fix_route.sh
+++ script_fix_route.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-_net="192.168.200.0"
+_net_list="192.168.254.0 192.168.255.0"
 _mask="255.255.255.0"
 
 while [[ true ]]; do
-    _gateway="$(sudo route -n | grep ppp0 | awk 'NR==1 {print $2}')"
-    if [ -n "$_gateway" ]; then
-        sudo route add -net $_net netmask $_mask gw $_gateway dev ppp0
+    _gateway="$(route -n | grep ppp0 | awk 'NR==1 {print $2}')"
+    if [[ "$_gateway" == "192.168."* ]]; then
+        for _net in $_net_list; do
+            route add -net $_net netmask $_mask gw $_gateway dev ppp0
+        done
         break
+    else
+        sleep 1
     fi
 done
